5da2ab0e84a0dd16cc7b49b52c44e6c9207e67fd
ViniciusGNunes <vini.gn14@gmail.com>
2025-10-02 19:37:30 -0700

    Added Model, Service, Controller and controller mapping methods

---
+++ Controllers/CalculationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitStartWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculationsController : ControllerBase
+    {
+    }
+}
---
+++ Models/CalorieCalcModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FitStartWebApi.Models
+{
+    public class CalorieCalcModel
+    {
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Gender { get; set; }
+    }
+}
