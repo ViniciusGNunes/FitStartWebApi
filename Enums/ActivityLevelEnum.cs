9d019e9e5bb3a079dde6162832cb033f2c0d4b93
ViniciusGNunes <vini.gn14@gmail.com>
2025-10-03 18:56:10 -0700

    Added the function to calculate the calories and its endpoint to the controller

---
+++ Enums/ActivityLevelEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace FitStartWebApi.Enums
+{
+    public enum ActivityLevelEnum
+    {
+        Sedentary = 1200,
+        Light = 1375,
+        Moderate = 1550,
+        Intense = 1725,
+        VeryIntense = 1900
+    }
+}
---
+++ Services/CalculationServices.cs
@@ -0,0 +1,47 @@
+﻿using FitStartWebApi.Enums;
+using FitStartWebApi.Models;
+
+namespace FitStartWebApi.Services
+{
+    public class CalculationServices
+    {
+        public double CalculateCalories(CalorieCalcModel info)
+        {
+            double Calories = 0;
+            double multiplier = 0;
+
+            switch (info.ActivityLevel)
+            {
+
+                case "Sedentary":
+                    multiplier = 1.2;
+                    break;
+                case "Light":
+                    multiplier = 1.375;
+                    break;
+                case "Moderate":
+                    multiplier = 1.55;
+                    break;
+                case "Intense":
+                    multiplier = 1.725;
+                    break;
+                case "Very Intense":
+                    multiplier = 1.9;
+                    break;
+            default:
+                break;
+            }
+
+            if(info.Gender == "Male")
+            {
+                Calories = 88.3 + (13.397 * info.Weight) + (4.799 * info.Height) - (5.677 * info.Age); 
+            }
+            else if (info.Gender == "Female")
+            {
+                Calories = 447.5 +(9.247 * info.Weight) + (3.098 * info.Height) - (4.330 * info.Age);
+            }
+
+            return Calories * multiplier;
+        }
+    }
+}
